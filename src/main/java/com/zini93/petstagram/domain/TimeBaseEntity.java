d550e5df223c56e0c7ab467b78a68a8af53686ba
ZINI93 <qwas566@gmail.com>
2025-05-28 12:49:17 +0900

    config

--- src/main/java/com/zini93/petstagram/domain/TimeBaseEntity.java
+++ src/main/java/com/zini93/petstagram/domain/TimeBaseEntity.java
@@ -1,17 +1,24 @@
 package com.zini93.petstagram.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class TimeBaseEntity {
 
     @CreatedDate
     @Column(name = "created_at", updatable = false)
-    private LocalDateTime crated_at;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "last_modified_at")
